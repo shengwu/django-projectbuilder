38db225ebe135f4c56d3a990d6f7e9989abef303
Kevin Xu <kevin@imkevinxu.com>
2012-12-10 06:11:24 -0800

    update to foundation 3.2.2

--- media-foundation/js/app.js
+++ media-foundation/js/app.js
@@ -10,15 +10,20 @@
   var $doc = $(document);
   var Modernizr = window.Modernizr;
 
-  $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-  $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-  $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-  $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-  $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-  $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-  $.fn.foundationTabs             ? $doc.foundationTabs() : null;
-  $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-  $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
+  });
 
   $("#featured").orbit();
   $('input, textarea').placeholder();
@@ -29,7 +34,7 @@
   // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
   // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
-  // Hide address bar on mobile devices
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
   if (Modernizr.touch) {
     $(window).load(function () {
       setTimeout(function () {
