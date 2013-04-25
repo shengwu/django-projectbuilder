f3b990c222bdcc8e7b954e288449fa04ca35a882
Kevin Xu <kevin@imkevinxu.com>
2013-04-24 20:36:55 -0700

    upgraded foundation media package to 4.1.2

--- media-foundation/js/app.js
+++ media-foundation/js/app.js
@@ -6,33 +6,11 @@
 
 
 
-  /* ZURB Foundation Javascript Initialization */
-  var $doc = $(document);
-  var Modernizr = window.Modernizr;
+  /* -----------------------------------------
+     ZURB FOUNDATION INITIALIZATION
+  ----------------------------------------- */
 
-  $(document).ready(function() {
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-  });
-
-  $("#featured").orbit();
-  $('input, textarea').placeholder();
-
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+  $(document).foundation();
 
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
   if (Modernizr.touch) {
