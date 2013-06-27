1d74dbcd6c738d877f423ac65eebdacbd5d20f47
Kevin Xu <kevin@imkevinxu.com>
2013-06-26 17:30:18 -0700

    collectstatic now compiles to assets/

--- djangobuilder.py
+++ djangobuilder.py
@@ -401,7 +401,6 @@ def build():
     cmd  = 'bash -c "source %s && workon' % VIRTUALENV_WRAPPER_PATH
     cmd += ' %(PROJECT_NAME)s && cd %(PROJECT_PATH)s &&' % replacement_values
     cmd += ' pip install -r requirements.txt && pip freeze > requirements.txt'
-    cmd += ' && python manage.py collectstatic --noinput"'
 
     output = sh(cmd)
 
